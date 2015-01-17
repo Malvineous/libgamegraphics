cf4c341a1bb1844b9e03af887814eabd2250e87f
Adam Nielsen <malvineous@shikadi.net>
2015-01-17 14:06:34 +1000

    Update headers for utils/*.cpp

--- utils/dumppal.cpp
+++ utils/dumppal.cpp
@@ -2,7 +2,7 @@
  * @file   dumppal.cpp
  * @brief  Extract the palette from a .png image such as a screenshot.
  *
- * Copyright (C) 2010-2011 Adam Nielsen <malvineous@shikadi.net>
+ * Copyright (C) 2010-2015 Adam Nielsen <malvineous@shikadi.net>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
